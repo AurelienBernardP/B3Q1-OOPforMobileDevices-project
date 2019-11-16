3cb3d80ac74d246912c7edc56d39d7359cfeef93
aurelienbernard <aurelienbernard@student.uliege.be>
2019-11-16 18:58:16 +0100

    added back and guide

--- lib/AdTreesAppTopBar.dart
+++ lib/AdTreesAppTopBar.dart
@@ -1,25 +1,24 @@
 import 'package:first/ItemList.dart';
 import 'package:flutter/material.dart';
-import 'package:admob_flutter/admob_flutter.dart';
+import 'Guide.dart';
 
 class AdTreesAppTopBar {
   final String text = "AdTrees";
   AppBar bar;
 
-  AdTreesAppTopBar(
-    String text,
-    BuildContext context,
-  ) {
+  AdTreesAppTopBar(String text,BuildContext context) {
     this.text.replaceAll('AdTrees', text);
+
     Widget settingsButton = IconButton(
       icon: Icon(Icons.settings),
       onPressed: () {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => SettingsScreen()),
+          MaterialPageRoute(builder: (context) => Guide()),
         );
       },
     );
+
     Widget inventoryButton = IconButton(
       icon: Icon(Icons.work),
       onPressed: () {
@@ -32,7 +31,8 @@ class AdTreesAppTopBar {
         );}
       },
     );
-Widget backButton = IconButton(
+
+    Widget backButton = IconButton(
         icon: Icon(Icons.arrow_back),
         onPressed: () => Navigator.pop(context),
       ),
@@ -45,43 +45,4 @@ Widget backButton = IconButton(
     this.bar = bar;
   }
   AppBar getBar() => bar;
-}
-
-class SettingsScreen extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("settings"),
-        ),
-        backgroundColor: Colors.green,
-        body: Container(
-          child: Container(
-            child: AdmobBanner(
-                adUnitId: 'ca-app-pub-3940256099942544/6300978111',
-                adSize: AdmobBannerSize.LARGE_BANNER,
-                listener: (AdmobAdEvent event, Map<String, dynamic> args) {
-                  switch (event) {
-                    case AdmobAdEvent.loaded:
-                      print('Admob banner loaded!');
-                      break;
-
-                    case AdmobAdEvent.opened:
-                      print('Admob banner opened!');
-                      break;
-
-                    case AdmobAdEvent.closed:
-                      print('Admob banner closed!');
-                      break;
-
-                    case AdmobAdEvent.failedToLoad:
-                      print(
-                          'Admob banner failed to load. Error code: ${args['errorCode']}');
-                      break;
-                    default:
-                      break;
-                  }
-                }),
-          ),
-        ));
-  }
 }
