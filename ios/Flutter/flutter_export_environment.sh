f3eec01f700936c69a784ca37dc37f7911e83030
Safadi <Tasnim.Safadi@student.uliege.be>
2019-12-02 14:36:53 +0200

    specifications added

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Alaa\Desktop\flutter_windows_v1.9.1+hotfix.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Alaa\OneDrive\Documents\ADTrees\info2051_2018"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Alaa\OneDrive\Documents\finalversion\info2051_2018"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
